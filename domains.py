cb7e47814c8526a93174abac90f4a26640579d6b
denizdiyep <denizdiyep@gmail.com>
2024-10-11 14:24:35 +0300

    sadece domainleri ekrana yaz

--- domains.py
+++ domains.py
@@ -5,4 +5,6 @@ import datetime
 with open('domains.csv') as myfile:
     myreader = csv.reader(myfile)
     for row in myreader:
-        print(row)
+        
+        mycolumns = row[0].split('|')
+        print(mycolumns[0])
