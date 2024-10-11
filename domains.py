1aee52ff3fac8a26c2e030c0e07dc3aed5422be1
denizdiyep <denizdiyep@gmail.com>
2024-10-11 15:55:54 +0300

    projeyi tamamladık

--- domains.py
+++ domains.py
@@ -5,6 +5,37 @@ import datetime
 with open('domains.csv') as myfile:
     myreader = csv.reader(myfile)
     for row in myreader:
-        
+        # gelen satırı sutunlara böl
         mycolumns = row[0].split('|')
-        print(mycolumns[0])
+
+        # bolunen sutunları ayrı degiskenlere ata
+        domain = mycolumns[0]
+        startdateStr = mycolumns[1]
+        enddateStr = mycolumns[2]
+        statusStr = mycolumns[3]
+        
+        # domain buyuk halini degiskene ata
+        bigDomain = domain.upper()
+        
+        # ihtiyaımız olan timezonelar degiskenlere atanır
+        utcTimezone = pytz.timezone('UTC')
+        turkeyTimezone = pytz.timezone('Europe/Istanbul')
+
+        # string tarih datetime nesnesine cevrilir
+        startdate = datetime.datetime.strptime(startdateStr, '%Y-%m-%d %H:%M')
+        enddate = datetime.datetime.strptime(enddateStr, '%Y-%m-%d %H:%M')
+
+        # string tarihi Türkiye zaman diliminde oldugunu soyledik
+        startdateTurkey = turkeyTimezone.localize(startdate)
+        enddateTurkey = turkeyTimezone.localize(enddate)
+
+        # Turkiye zaman diliminde olan deger UTC olarak cevrildi
+        startdateTurkey = startdateTurkey.astimezone(utcTimezone)
+        enddateTurkey = enddateTurkey.astimezone(utcTimezone)
+        
+        if statusStr == 'Aktif':
+            status = 'ayşe'
+        else:
+            status = 'fatma'
+
+        print(status)
